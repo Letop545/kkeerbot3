0db4b75182d6bc3f75d156592f3a8e29400136c7
Letop545 <71585818+Letop545@users.noreply.github.com>
2020-09-20 17:35:11 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: "NzU3MTQ0MzE0MDA1NDIyMTgy.X2cHlw.Py45wSa33jz53h9CK674lhkPlbE",
+    prefix: "/",
+    owner: "727945892883333191",
+    corona_api: "https://corona.lmao.ninja/v2"
+    }
